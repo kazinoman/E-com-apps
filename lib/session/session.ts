f4d3fe2e9684b19711b8ed55ab9bcb5e83621be1
Abdullah al Noman <kazinoman9999@gmail.com>
2026-06-14 00:20:16 +0600

    fix build issue

--- lib/session/session.ts
+++ lib/session/session.ts
@@ -1,6 +1,6 @@
 // lib/server/session.ts
 import { cookies } from "next/headers";
 
-export function getSession() {
-  return cookies().get("buyer_session")?.value;
-}
+// export function getSession() {
+//   return cookies().get("buyer_session")?.value;
+// }
